16dce24234aed59249f32ec44f14046e8f838353
cactacaea <kristina.krivova@ucas-edu.net>
2025-08-25 10:50:04 -0600

    who are you project

--- projects/who_are_you.py
+++ projects/who_are_you.py
@@ -1,9 +1,15 @@
 # KK 2nd Who Are You
 
-name = input("What's your name?:\n").capitalize().strip()
+saved_names=[]
+name = input("What's your name?:\n\n").capitalize().strip()
 age = input("How old are you?:\n")
 color = input("Lastly, what's your favourite color?:\n")
-print("")
+if name in saved_names:
+   print(f"Welcome back, {name}!")
+else:
+    print(f"You are {age} years old and your favourite color is {color}, {name}!")
+saved_names.append(name)
+    
 
 
 
