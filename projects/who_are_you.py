cce2f9ab40a6f5fb9ab621cc142a6b9b3a703a9f
cactacaea <kristina.krivova@ucas-edu.net>
2025-08-27 10:25:30 -0600

    who are you

--- projects/who_are_you.py
+++ projects/who_are_you.py
@@ -1,21 +1,17 @@
 # KK 2nd Who Are You
 
-saved_names=[]
-name = input("What's your name?:\n\n").capitalize().strip()
-age = input("How old are you?:\n")
-color = input("Lastly, what's your favourite color?:\n")
-if name in saved_names:
-   print(f"Welcome back, {name}!")
-else:
-    print(f"You are {age} years old and your favourite color is {color}, {name}!")
-saved_names.append(name)
+while True:
+    saved_names=[]
+    name = input("What's your name?:\n").capitalize().strip()
     
+    if name in saved_names:
+        print(f"Welcome back, {name}!")
+    else:
+        age = input("How old are you?:\n")
+        color = input("Lastly, what's your favourite color?:\n")
+        print(f"You are {age} years old and your favourite color is {color}, {name}!")
+    saved_names.append(name)
 
-
-
-
-
-
-
-
-
+    cont = input("\nWould you like to continue?:\n")
+    if cont != "yes":
+        break
