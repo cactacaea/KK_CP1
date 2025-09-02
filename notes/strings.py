8444f02e917066c53105a57cdba60e04e2dbf22e
cactacaea <kristina.krivova@ucas-edu.net>
2025-09-02 10:51:24 -0600

    strings notes

--- notes/strings.py
+++ notes/strings.py
@@ -5,4 +5,30 @@ my_dictionary = {"name": "kris",
                  "age": "99",
                  "class": "CS",}
 print(my_dictionary['age'])
+# strings & examples
+"content of string"
+'content of string'
 
+"The quick brown fox jumps over the lazy dog!" 
+# cannot access this string since it's not stored in a varible ^
+sentence = "The quick brown fox jumps over the lazy dog!" 
+first_name = 'Xavier'
+last_name = "LaRose"
+month = 'September'
+food = "ramen"
+# if " or ' is needed 
+name = "La'Rose"
+best = '"The Return of the King" is the best book ever!'
+
+funny = "The dog jumped into the lake"
+# string length
+length = len(month)
+print(f"September has{length} letters!")
+# escape characters
+print('\tWill said \ndon\'t die')
+# concatenation
+full_name = first_name + " " + last_name
+print(name)
+# slicing strings
+# string[start index, end index]
+print(last_name[2:5])
--- practices/madlib.py
+++ practices/madlib.py
@@ -1,5 +1,8 @@
 # KK 2nd Madlib Practice
 
+year_number = input("Choose a number: ")
+substance = input("Choose a substance: ")
+adjective = input("Choose an adjective: ")
 
 print(f"After NUMBER years of toxic SUBSTANCES, Earth is finally habitable, maybe even ADJECTIVE; completely and utterly shocking.")
 print(f"Rather than coming up with some genius plan, we as a species figured it'd be great to communicate with PLURAL ANIMAL instead.")
