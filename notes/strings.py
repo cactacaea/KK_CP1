263a39318deca88a5f64299640f5c36c759c44c0
cactacaea <kristina.krivova@ucas-edu.net>
2025-09-01 22:53:41 -0600

    madlib text and notes sync

--- notes/strings.py
+++ notes/strings.py
@@ -1,2 +1,8 @@
 # KK 2nd Strings Notes
 
+# dictionaries
+my_dictionary = {"name": "kris",
+                 "age": "99",
+                 "class": "CS",}
+print(my_dictionary['age'])
+
---
+++ practices/madlib.py
@@ -0,0 +1,10 @@
+# KK 2nd Madlib Practice
+
+
+print(f"After NUMBER years of toxic SUBSTANCES, Earth is finally habitable, maybe even ADJECTIVE; completely and utterly shocking.")
+print(f"Rather than coming up with some genius plan, we as a species figured it'd be great to communicate with PLURAL ANIMAL instead.")
+print(f"NUMBER years of advancement in technology.. yet we're using PLURAL HOUSEHOLD APPLIANCE to communicate.")
+print(f"Although, technology allowed us to hear the first phrase from PLURAL ANIMAL.. 'SILLY PAHRASE'")
+print(f"On a cold TIMEOFDAY/at sharply TIME o clock - scientists would build a ADJECTIVE HOUSEHOLD APPLAINCE that exploded when touching a/an NOUN")
+print(f"How odd. Our home (a giant rock in space) may be habitable -- but slightly too ADJECTIVE for comfort")
+
