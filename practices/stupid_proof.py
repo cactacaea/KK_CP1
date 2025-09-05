e571d0335e05af48bb61a8bf2520e9c43f03222d
cactacaea <kristina.krivova@ucas-edu.net>
2025-09-05 10:51:40 -0600

    stupid proofing

--- practices/stupid_proof.py
+++ practices/stupid_proof.py
@@ -1,1 +1,18 @@
 # KK 2nd Stupid Proofing Practice
+
+print("Greetings!\n")
+while True:
+    print("hi")
+    firstname = input("What's your first name?:\n").capitalize().strip()
+    if firstname.isalpha():
+        break
+    else:
+        print("Nuh uh. Type actual letters, silly.\n")
+    lastname = input("What's your last name?:\n").capitalize().strip()
+    if lastname.isalpha():
+        break
+    else:
+        print("Let's try this again.. if you put your last name on a passport, what would it look like?\n")
+    phone = input("What's your phone number?:\n")
+
+
