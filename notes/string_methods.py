36002664c5b8bf12516be249fb2126cc6725ebc8
cactacaea <kristina.krivova@ucas-edu.net>
2025-09-05 20:48:18 -0600

    stupid proof

--- notes/string_methods.py
+++ notes/string_methods.py
@@ -1,25 +1,27 @@
 # KK 2nd String Methods Notes
 
-name = input("What's your name?: ").strip().title()
-age = int(input("How old are you?: "))
+# name = input("What's your name?: ").strip().title()
+# age = int(input("How old are you?: "))
 
-# .lower() > makes all characters lowercase
-# .upper() > makes all characters uppercase
-# .capitalize() > capitalizes the first letter of the string
-# .title() > capitalizes the first letter of every word
+# # .lower() > makes all characters lowercase
+# # .upper() > makes all characters uppercase
+# # .capitalize() > capitalizes the first letter of the string
+# # .title() > capitalizes the first letter of every word
 
-#print("Hello {}! Nice to meet you. It's crazy that you are {:.2f} years old.".format(name,age))
-print(f"Hello {name}! Nice to meet you. It's crazy that you are {age} years old.")
+# #print("Hello {}! Nice to meet you. It's crazy that you are {:.2f} years old.".format(name,age))
+# print(f"Hello {name}! Nice to meet you. It's crazy that you are {age} years old.")
 
 # print(age.isdigit())
 # print(f"Really? {age} is old.")
 
+# finds the starting letter of the word in the sentence and prints the word if it can find it
 sentence = "The quick brown fox jumps over the lazy dog!"
-#print(sentence.find("fox"))
-# prints the word if it can find it in sentence
+print(sentence.find("over"))
+
 word = "brown"
 start = sentence.find(word)
 length = len(word)
-#print(sentence[start:start+length])
+print(sentence[start:start+length])
+
 # replaces the word and re-prints the sentence
 print(sentence.replace(word, "orange"))
--- practices/stupid_proof.py
+++ practices/stupid_proof.py
@@ -1,18 +1,43 @@
-# KK 2nd Stupid Proofing Practice
+# KK 2nd Idiot Proof Practice
 
-print("Greetings!\n")
+# intro & first name loop (keeps asking until the input has only letters)
+print("\nGreetings!\n")
 while True:
-    print("hi")
+    #print("Hellow")
     firstname = input("What's your first name?:\n").capitalize().strip()
     if firstname.isalpha():
         break
     else:
         print("Nuh uh. Type actual letters, silly.\n")
+
+# last name loop (keeps asking until the input has only letters)
+while True:
     lastname = input("What's your last name?:\n").capitalize().strip()
     if lastname.isalpha():
         break
     else:
         print("Let's try this again.. if you put your last name on a passport, what would it look like?\n")
-    phone = input("What's your phone number?:\n")
 
+# phone number loop (keeps asking until the input is 10 letters)
+while True:
+    phone = input("What's your phone number?:\n").strip()
+    length_phone = len(phone)
+    if phone.isdigit() and length_phone == 10:
+        break
+    else:
+        print("Try again please. This time type 10 numbers.\n")
 
+# gpa loop (keeps asking until input is a digit/float)
+while True:
+    gpa = float(input("What's your GPA this term?:\n"))
+    if gpa == float(gpa):
+        
+        break
+    else: 
+        print("Nope, only numbers are allowed here.\n")
+
+# outputs
+print("\n Cool cool cool. Here is your data!:\n")
+print("Name:", firstname, lastname)
+print("Phone:", phone[0:3], phone[3:6], phone[6:10])
+print("GPA:", round(gpa, 1))
