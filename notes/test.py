74076060f5473e2708c43ddbe3aa181c0b04f698
cactacaea <kristina.krivova@ucas-edu.net>
2025-09-09 16:26:55 +0000

    test doc changes

--- notes/test.py
+++ notes/test.py
@@ -1,6 +1,6 @@
 # KK 2nd Test File
 
 print("test\n")
-# print() <= everything in the parenthesis appears in the terminal
+# !
 print("Practice sync and hw")
-
+print("I added this text on GitHub codespaces.")
