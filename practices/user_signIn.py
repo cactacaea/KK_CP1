05f99d47ed6b4dae69355bb6000abc4b290d0b1e
cactacaea <kristina.krivova@ucas-edu.net>
2025-09-23 23:09:23 -0600

    finished user sign in practice

--- practices/user_signIn.py
+++ practices/user_signIn.py
@@ -1,17 +1,30 @@
 # KK 2nd User Sign In Practice
 
-while True:
-    user = input("Please choose a username:\n")
-    password = input("Please choose a password:\n")
-    users = [["LaRose","programmer246"],["cactus","18passwords"],[["cactacaea"],["123"]]]
+# user = input("Please choose a username:\n")
+# password = input("Please choose a password:\n")
+# users = [["LaRose","abcd"],["cactus","password"],[["cactacaea"],["123"]]]
 
-    user_checker = input("Please re-enter a username:\n")
-    if user_checker in users:
-        continue
+# while True:
+#     user_checker = input("Please re-enter a username:\n")
+#     if user_checker in users or user:
+#         password_checker = input("Please re-enter the password:\n")
+#         if password_checker in users or password:
+#             print(f"Welcome, {user}!\n")
+#             continue
+#         else:
+#             print("Password is invalid; please try again\n")
+#     else:
+#         print("Username is invalid; please try again")
+
+user = input("Please choose a username:\n")
+password = input("Please choose a password:\n")
+print("Thank you.")
+usr_checker = input("Re-enter your username to sign in:\n")
+if usr_checker == user:
+    pass_checker = input("Re-enter your password to sign in:\n")
+    if pass_checker == password:
+        print(f"Welcome, {user}!")
     else:
-        print("Username is invalid; please try again\n")
-    password_checker = input("Please re-enter the password:\n")
-    if password_checker in users:
-        print(f"Welcome, {user}!\n")
-    else:
-        print("Password is invalid; please try again")
+        print("Incorrect password, please try again.")
+else:
+    print("Incorrect username, please try again.")
