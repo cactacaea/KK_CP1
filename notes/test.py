961b2c4e12bd92ca836362ee9f6d92fde3572863
cactacaea <kristina.krivova@ucas-edu.net>
2025-09-30 00:36:10 -0600

    shopping list

--- notes/test.py
+++ notes/test.py
@@ -9,3 +9,8 @@ print(a[1:4])
 print(float(3))
 print('3.0')
 print(-3.5%2)
+
+my_list = [1, 2, 3]
+input = input("Enter something: ")
+my_list.append(input)
+print(my_list)
--- practices/shopping_list.py
+++ practices/shopping_list.py
@@ -1,3 +1,23 @@
 # KK 2nd Shopping List Manager Practice
 
-added_item = input("What would you like to add to your shopping list?:\n")
+start = '\033[1m'
+end = '\033[0m'
+
+shopping_list = []
+print("Greetings. Type 'exit' to stop.")
+while True:
+    choice = input(f"{start}'Add' - Add an item to your shopping list\n'Remove' - Remove an item from your shopping list\n'View' - See what items you have in your shopping list\n'Check' - Mark an item as bought in your shopping list\n{end}Respond below to remove something, add, or see your list:\n")
+    
+    if choice == "add":
+        added_item = input("\nWhat would you like to add to your shopping list?:\n").capitalize().strip()
+        shopping_list.append(added_item)
+        #print(f"{shopping_list}\n")
+        
+        for item in shopping_list:
+            print(f"\nNoted Shopping List:\n{item}")
+
+    
+    if added_item == "exit":
+        break
+
+
