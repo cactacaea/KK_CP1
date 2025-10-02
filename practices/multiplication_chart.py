0e15371ea9f1b46785fdb313361f2ab467274543
cactacaea <kristina.krivova@ucas-edu.net>
2025-10-01 23:16:24 -0600

    multpl. chart

--- practices/multiplication_chart.py
+++ practices/multiplication_chart.py
@@ -1,3 +1,15 @@
 # KK 2nd Multiplication Table Practice
 
-base = int(input("What base would you like to use for your multiplication table? Ex; base 12:\n"))
+import time
+
+base = int(input("What base would you like to use for your multiplication table? Ex; 12:\n"))
+print(f"Multiplication Chart: {base}x{base}\n")
+time.sleep(2)
+
+for num in range(1,base+1):
+    for another in range(1,base+1):
+        #if num < 10:
+        print(f"{num*another}")#parameter?)
+        #else:
+    print()
+
