c75a3d1777354ae00d56b3c534e48a12815e39b3
cactacaea <kristina.krivova@ucas-edu.net>
2025-10-02 10:51:24 -0600

    multiplication chart

--- practices/multiplication_chart.py
+++ practices/multiplication_chart.py
@@ -4,12 +4,9 @@ import time
 
 base = int(input("What base would you like to use for your multiplication table? Ex; 12:\n"))
 print(f"Multiplication Chart: {base}x{base}\n")
-time.sleep(2)
+time.sleep(1)
 
 for num in range(1,base+1):
     for another in range(1,base+1):
-        #if num < 10:
-        print(f"{num*another}")#parameter?)
-        #else:
+        print(f"{num*another}".center(4), end="\t")
     print()
-
