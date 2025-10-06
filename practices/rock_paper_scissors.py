c3f2bdbce94279890eabd154a0928c148a5a44e0
cactacaea <kristina.krivova@ucas-edu.net>
2025-10-06 01:24:08 -0600

    rps practice

--- practices/rock_paper_scissors.py
+++ practices/rock_paper_scissors.py
@@ -4,6 +4,20 @@ start = '\033[1m'
 end = '\033[0m'
 
 print("Greetings.\nType 'q' to stop playing")
+choice_list = ["rock","paper","scissors"]
+computer_points = 0
+player_points = 0
 
 while True:
     choice = input(f"\n{start}Rock: 'r'\nPaper: 'p'\nScissors: 's'{end}\nWhat would you like to play?:\n").strip().lower()
+    if choice == "q":
+        print("\nThanks for playing!")
+        break
+    elif choice == "r":
+        print()
+    elif choice == "p":
+        print()
+    elif choice == "s":
+        print()
+    else:
+        print("\nInvalid choice! Please try again")
