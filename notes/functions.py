ddb715b4abdbea3d127431ccb573cb8ff2467bd5
cactacaea <kristina.krivova@ucas-edu.net>
2025-10-20 10:52:57 -0600

    functions notes

---
+++ notes/functions.py
@@ -0,0 +1,14 @@
+# KK 2nd Functions Notes
+
+num = 0
+def add(x,y):
+    return x+y
+
+while num < add(5,5):
+    print("duck!")
+    num += 1
+print("goose!")
+print(f"results are: {add(220,848)}")
+total = add(8535394,9981)
+print(add(add(3.14,.85),10))
+add(42,7)
