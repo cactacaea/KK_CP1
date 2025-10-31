c760f4c18e5c5463c7e65c7c6a0d2bf421e6a698
cactacaea <kristina.krivova@ucas-edu.net>
2025-10-31 16:13:49 -0600

    almost finished turtle race

--- practices/turtle_race.py
+++ practices/turtle_race.py
@@ -24,11 +24,14 @@ maurice.shape("turtle")
 albert.shape("turtle")
 # raceSetup FUNCTION
 def raceSetup():
-    # draw a finish line around 0,0
+    # draw a finish line after 0,0
     drawer = turtle.Turtle()
     drawer.speed(10)
     drawer.color("#292929")
-    drawer.pensize(8)
+    drawer.pensize(10)
+    drawer.penup()
+    drawer.forward(700)
+    drawer.pendown()
     drawer.left(90)
     drawer.forward(800)
     drawer.backward(800)
@@ -37,59 +40,72 @@ def raceSetup():
     drawer.penup()
     # start turtles on the same x coordinate (-850,y)
     gary.penup()
-    gary.speed(13)
+    gary.speed(10)
     gary.goto(-850, 400)
     gary.color("#C50303")
     gary.pensize(6)
     gary.pendown()
 
     tim.penup()
-    tim.speed(13)
+    tim.speed(10)
     tim.goto(-850, 200)
     tim.color("#97D161")
     tim.pensize(6)
+    tim.pendown()
 
     boris.penup()
-    boris.speed(13)
+    boris.speed(10)
     boris.goto(-850, 0)
     boris.color("#E4760F")
     boris.pensize(6)
+    boris.pendown()
 
     maurice.penup()
-    maurice.speed(13)
+    maurice.speed(10)
     maurice.goto(-850, -200)
     maurice.color("#618ED1")
     maurice.pensize(6)
+    maurice.pendown()
 
     albert.penup()
-    albert.speed(13)
+    albert.speed(10)
     albert.goto(-850, -400)
     albert.color("#DC91FA")
     albert.pensize(6)
+    albert.pendown()
     
-    time.sleep(3)
+    time.sleep(1)
+
+    gary.speed(4)
+    tim.speed(4)
+    boris.speed(4)
+    maurice.speed(4)
+    albert.speed(4)
 # moveTurtle FUNCTION with parameters for each turtle
 
-    # determine random integer # of steps
-# win FUNCTION with parameters for each turtle, conditionals to check each turtle, parameter of the user's bet
-    # if user's bet is the same as the turtle that won, display a positive message
-
-
 raceSetup()
-random_step = random.randint(50)
+# determine random integer number of steps
+random_step = random.randint(0,50)
 
-points = [gary.position(), tim.position(), boris.position(), maurice.position(), albert.position()]
-wanted_x = 0
-for point in points:
-    if point[0] == wanted_x:
-        x_coor = 0
-# while true loop until a turtle reaches a coordinate
-while True:
+game = True
+# while true loop until a turtle reaches a coordinate for win "function"
+while game:
     for step in range(850):
         gary.forward(random_step)
+        if gary.xcor()>= 700:
+            print("Gary won")
+            game = False
+            break
         tim.forward(random_step)
         boris.forward(random_step)
         maurice.forward(random_step)
         albert.forward(random_step)
-    if gary.position() or tim.position() or boris.position() or maurice.position() or albert.position() == x_coor:
-        break
+
+    # points = [gary.position(), tim.position(), boris.position(), maurice.position(), albert.position()]
+    # wanted_x = 0
+    # for point in points:
+    #     if point[0] == wanted_x:
+    #         print("a turtle won")
+    #         break
+    # if gary.position() or tim.position() or boris.position() or maurice.position() or albert.position() == x_coor:
+    #     break
