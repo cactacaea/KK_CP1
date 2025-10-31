8f89a764a48c7c6442e3025742bacf694053890f
cactacaea <kristina.krivova@ucas-edu.net>
2025-10-31 10:51:12 -0600

    unfinished running turtle race and maze gen

---
+++ practices/maze_generator.py
@@ -0,0 +1,1 @@
+# KK 2nd Maze Generator Pracice
--- practices/turtle_race.py
+++ practices/turtle_race.py
@@ -8,46 +8,68 @@ import time
 
 # 5 turtles named gary, tim, boris, maurice, and albert using turtle.Turtle()
 gary = turtle.Turtle()
-tim = turtle.Turtle().shape("turtle")
-boris = turtle.Turtle().shape("turtle")
-maurice = turtle.Turtle().shape("turtle")
-albert = turtle.Turtle().shape("turtle")
+tim = turtle.Turtle()
+boris = turtle.Turtle()
+maurice = turtle.Turtle()
+albert = turtle.Turtle()
 # screen setup/pixels/title
 screen = turtle.Screen()
 screen.setup(2000,1100)
 screen.title("Turtle Race")
 
 gary.shape('turtle')
+tim.shape("turtle")
+boris.shape("turtle")
+maurice.shape("turtle")
+albert.shape("turtle")
 # raceSetup FUNCTION
 def raceSetup():
     # draw a finish line around 0,0
-    turtle.color("#292929")
-    turtle.pensize(8)
-    turtle.left(90)
-    turtle.forward(800)
-    turtle.backward(800)
-    turtle.backward(800)
+    drawer = turtle.Turtle()
+    drawer.speed(10)
+    drawer.color("#292929")
+    drawer.pensize(8)
+    drawer.left(90)
+    drawer.forward(800)
+    drawer.backward(800)
+    drawer.backward(800)
     # penup
-    #turtle.penup()
+    drawer.penup()
     # start turtles on the same x coordinate (-850,y)
+    gary.penup()
+    gary.speed(13)
     gary.goto(-850, 400)
-    gary.color("#D16262")
+    gary.color("#C50303")
     gary.pensize(6)
+    gary.pendown()
+
+    tim.penup()
+    tim.speed(13)
     tim.goto(-850, 200)
     tim.color("#97D161")
     tim.pensize(6)
+
+    boris.penup()
+    boris.speed(13)
     boris.goto(-850, 0)
     boris.color("#E4760F")
     boris.pensize(6)
+
+    maurice.penup()
+    maurice.speed(13)
     maurice.goto(-850, -200)
     maurice.color("#618ED1")
     maurice.pensize(6)
+
+    albert.penup()
+    albert.speed(13)
     albert.goto(-850, -400)
-    albert.color("#B61EF1")
+    albert.color("#DC91FA")
     albert.pensize(6)
-    #turtle.pendown()
-    time.sleep(5)
+    
+    time.sleep(3)
 # moveTurtle FUNCTION with parameters for each turtle
+
     # determine random integer # of steps
 # win FUNCTION with parameters for each turtle, conditionals to check each turtle, parameter of the user's bet
     # if user's bet is the same as the turtle that won, display a positive message
@@ -69,5 +91,5 @@ while True:
         boris.forward(random_step)
         maurice.forward(random_step)
         albert.forward(random_step)
-    if gary.position or tim.position or boris.position or maurice.position or albert.position == x_coor:
+    if gary.position() or tim.position() or boris.position() or maurice.position() or albert.position() == x_coor:
         break
