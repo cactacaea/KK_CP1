bbdb2584a3280fc8d35146a576b27e1f15707d23
cactacaea <kristina.krivova@ucas-edu.net>
2025-11-03 16:01:18 -0700

    maze generator

--- practices/maze_generator.py
+++ practices/maze_generator.py
@@ -6,8 +6,8 @@ import random
 import time
 
 # 2 nested list maze grid variable w/ 6 lists inside one list and 6 more lists inside each list
-column_grid = [[[],[],[],[],[],[]],
-               [[],[],[],[],[],[]],
+column_grid = [[True,True,True,True,True,True],
+               [True,True,True,True,True,True],
                [[],[],[],[],[],[]],
                [[],[],[],[],[],[]],
                [[],[],[],[],[],[]],
@@ -30,41 +30,30 @@ def setup():
     turtle.pensize(4)
     turtle.speed(6)
     turtle.shape("turtle")
-    turtle.penup()
-    turtle.goto(-400,400)
-    turtle.pendown()
     
-# maze wall drawing function(?)
-#def mazeWall():
-
-# right vertical line
-# for loop iterating over each list in the column grid list
-turtle.right(90)
-for list in column_grid:
-    # nested for loop iterating over each first list in the previous lists
-    for nested_list in list:
-        turtle.forward(20)
-
-# left vertical line
-
-# turtle.penup()
-# turtle.goto(400,400)
-# turtle.pendown()
-
-# for loop iterating over each list in the maze list
-for list in column_grid:
-    # nested for loop iterating over the last index of each previous list
-    for nested_list in list:
-        turtle.forward(20)
-
-# bottom line
-# for loop iterating over the last list in the first maze list
-    # nested for loop drawing a line to the right for each list in the last list
-
-# top line
-# for loop taking the first list in the initial entire list
-    # nested for loop drawing a line to the right for every list
+# maze wall drawing FUNCTION
+def mazeWall():
+    # TOP LINE
+    # turtle starts in the 2nd quadrant
+    turtle.penup()
+    turtle.goto(-360,360)
+    turtle.pendown()
+    # draws 20 pixels for every slot in the grid
+    turtle.forward(720)
+    time.sleep(1)
+    turtle.right(90)
+    turtle.forward(720)
+    turtle.right(90)
+    turtle.forward(720)
+    time.sleep(1)
+    turtle.right(90)
+    turtle.forward(720)
 
 # drawing/generating the actual maze FUNCTION
+# def drawMaze():
+
+# check for solvable maze FUNCTION
+
 
 setup()
+mazeWall()
