1cf926d94db74e9410ccb1e02cd735761d366ec2
cactacaea <kristina.krivova@ucas-edu.net>
2025-11-12 10:50:57 -0700

    order up practice in progress

--- practices/order_up.py
+++ practices/order_up.py
@@ -18,16 +18,70 @@ sides = {
     "Tater Tots": 2.00
 }
 
-# order process FUNCTION with parameters for what dictionary is being checked and the entre
-    # finished dictionary (empty in the beginning, added keys/values after input)
+# empty final bill dictionary
+finished_bill = {}
+
+# order process FUNCTION with parameters for what dictionary is being checked and the final bill
+def order(dictionary,finished_bill):
     # while LOOP until the user enters a valid item from 1 of 3 menus
+    while True:
         # prompt/input
+        
         # if input isn't in drink dictionary, ask again
         # else break out if it's valid
 
-    # RETURN MODIFIED FINISHED DICITONARY
+    # RETURN MODIFIED BILL
 
 # introduction explaining what to type for different actions: quit, drinks, main course, sides, tax
 print("Greetings! Type:\n'Quit' to stop ordering\n'Drinks' to order a drink and see the drinks menu\n'Entrees' to order your main course and see the entree menu\n'Sides' to order 2 side dishes and see the menu")
-tax = input("\nFirst off, we must take note of tax to help the economy. What is the tax percentage where you live?:\n")
-# call order function 3 times w/ each menu as a parameter
+tax = input("\nFirst off, we must take note of tax to help the economy. What is the tax percentage where you live? Enter the plain number w/o a percent symbol\nEx: 4.55\n")
+while True:
+    choice = input("Type:\n'Quit' to stop ordering\n'Drinks' to order a drink and see the drinks menu\n'Entrees' to order your main course and see the entree menu\n'Sides' to order 2 side dishes and see the menu\nWhat would you like to do?:").lower().strip()
+    # call order function w/ each menu as a parameter depending on what the user chose
+    if choice == "quit":
+        break
+    elif choice == "drinks":
+
+        order(drinks,finished_bill)
+    elif choice == "entrees":
+        order(main_courses,finished_bill)
+    elif choice == "sides":
+        order(sides,finished_bill)
+    else:
+        print("Invalid choice, silly. Pick again.")
+
+
+# introduction explaining different actions: quit, tax, order, see each of the 3 menus
+print("Welcome to Fredbear's Family Diner!")
+# tax
+tax = input("\nTo begin, we must take note of tax to help the economy. What is the tax percentage where you live? Enter the plain number w/o a percent symbol\nEx: 4.55\n")
+# while LOOP so the user enters a valid option
+while True:
+    choice = input("Type:\n'Quit' to stop ordering\n'Order' to place your order\n'Drinks' to view the drinks menu\n'Entrees' to view the main courses menu\n'Sides' to view the side dishes menu\n\nWhat would you like to do?:\n").lower().strip()
+    # if user picks to quit, stop the code
+    if choice == "quit":
+        break
+    # if user picks to see drinks menu, make a for LOOP showing the drinks menu
+    elif choice == "drinks":
+
+    # if user picks the entrees menu, print the entrees menu nicely using a for LOOP
+    elif choice == "entrees":
+
+    # if user picks the sides menu, print the sides menu nicely using a for LOOP
+    elif choice == "sides":
+
+    # if user picks to order, ask which menu they would like to order from
+    elif choice == "order":
+        while True:
+            menu_choice = input("'Drinks': order a drink\n'Entrees': order a main dish\n'Sides': order 2 side dishes\nWhat menu would you like to order from?:\n").lower().strip()
+            if menu_choice == "drinks":
+                order(drinks,finished_bill)
+            elif menu_choice == "entrees":
+                order(main_courses,finished_bill)
+            elif menu_choice == "sides":
+                order(sides,finished_bill)
+            else:
+                print("Choose a valid option >:(")
+    # else, ask again
+    else:
+        print("Invalid choice, silly. Pick again")
