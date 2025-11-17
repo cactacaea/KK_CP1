e7f22f11505b2c2a11450cf796153c62b6f672e9
cactacaea <kristina.krivova@ucas-edu.net>
2025-11-17 10:48:55 -0700

    args and kwargs notes and practice proj

---
+++ notes/arg_kwargs.py
@@ -0,0 +1,22 @@
+# KK 2nd *args and **kwargs Notes
+
+# def hello(age = 29,name = "Tia"):
+#     return(f"Hello {name}! You are {age}.")
+
+# print(hello())
+# print(hello("Xavier"))
+# print(hello(name = "Treyson",age = 19))
+
+# args are when we don't know how many positional arguments to give/there are
+
+# positional arguments, *args, keyword arguments, **kwargs
+def hello(*names, **last):
+    print(type(names))
+    print(last)
+    for name in names:
+        if name == "Vienna":
+            print(f"Hello {name} {last['alast']} {last['v_last']}")
+        else:
+            print(f"Hello, {name} {last['alast']}")
+
+hello("Alexander","Kathryn","Andrew","Vienna","Victoria","Treyson","Xavier","Jake", alast = "LaRose", v_last = "Atkin")
---
+++ practices/flexible_calculator.py
@@ -0,0 +1,3 @@
+# KK 2nd, Flexible Calculator // Practice
+
+# greet user with available operations and input for what operation they would like to use
