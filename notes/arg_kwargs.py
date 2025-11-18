7699c2e57c51ee9d8efefe31994be0f05f4b52fc
cactacaea <kristina.krivova@ucas-edu.net>
2025-11-18 10:51:20 -0700

    broken flexible calculator

--- notes/arg_kwargs.py
+++ notes/arg_kwargs.py
@@ -1,16 +1,16 @@
 # KK 2nd *args and **kwargs Notes
 
-# def hello(age = 29,name = "Tia"):
-#     return(f"Hello {name}! You are {age}.")
+def hello(age = 29,name = "Tia"):
+    return(f"Hello {name}! You are {age}.")
 
-# print(hello())
-# print(hello("Xavier"))
-# print(hello(name = "Treyson",age = 19))
+print(hello())
+print(hello("Xavier"))
+print(hello(name = "Treyson",age = 19))
 
 # args are when we don't know how many positional arguments to give/there are
 
 # positional arguments, *args, keyword arguments, **kwargs
-def hello(*names, **last):
+def hello2(*names, **last):
     print(type(names))
     print(last)
     for name in names:
@@ -19,4 +19,4 @@ def hello(*names, **last):
         else:
             print(f"Hello, {name} {last['alast']}")
 
-hello("Alexander","Kathryn","Andrew","Vienna","Victoria","Treyson","Xavier","Jake", alast = "LaRose", v_last = "Atkin")
+hello2("Alexander","Kathryn","Andrew","Vienna","Victoria","Treyson","Xavier","Jake", alast = "LaRose", v_last = "Atkin")
--- practices/flexible_calculator.py
+++ practices/flexible_calculator.py
@@ -1,5 +1,39 @@
 # KK 2nd, Flexible Calculator // Practice
 
-# greet user with available operations and input for what operation they would like to use
+
+# RETURN VALUE BACK TO FUNCTION WHEN PRINTING
+# sum function w/ args parameter
+# def sum(*values):
+
+
+# # average function w/ args parameter
+# def average():
+
+# # maximum function w/ args parameter
+# def maximum():
+
+# # minimum function w/ args parameter
+# def minimum():
+
+# # multiplying/product function w/ args parameter
+# def product():
+
+
+# greet user with available operations 
+print("Greetings! This is a flexible calculator. The available operations are:\nSum\nAverage\nMax/Maximum\nMin/Minimum\nProduct\n")
+# input for what operation user would like to use 
+operation_opt = input("What operation would you like to perform?:\n")
+print("\nEnter the numbers you would like to perform the operation on; type 'stop' to calculate.")
+# while LOOP for input of numbers until the user decides to stop
+chosen_numbers = []
+while True:
+    # append to a list after each number
+    num_choice = input("Number: ")
+    chosen_numbers.append(num_choice)
+    if num_choice == "stop":
+        break
+    else:
+        continue
 
 # conditionals checking what the user gave and calling the designated function depending on the operation
+# PRINT RETURNED VALUE BY MAKING THE FUNCTION CALL BE A VARIABLE
