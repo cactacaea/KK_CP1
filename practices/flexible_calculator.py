d767b4df85455b956f3d224816577bb0c24de58a
cactacaea <kristina.krivova@ucas-edu.net>
2025-11-18 10:18:35 -0700

    testing testing 123

--- practices/flexible_calculator.py
+++ practices/flexible_calculator.py
@@ -1,3 +1,5 @@
 # KK 2nd, Flexible Calculator // Practice
 
 # greet user with available operations and input for what operation they would like to use
+
+# conditionals checking what the user gave and calling the designated function depending on the operation
