6b0674734c802b765e06f22f54bc275be76c6f77
cactacaea <kristina.krivova@ucas-edu.net>
2025-12-08 10:52:15 -0700

    adv game

--- projects/adventure_game_final.py
+++ projects/adventure_game_final.py
@@ -28,28 +28,106 @@ location_names = {
 }
 
 def main():
+    current_loc = Locations.SPAWN
+    bold = '\033[1m'
+    end = '\033[0m'
+    playername = input("Welcome to the Retreat Island! First off we need to get to know you. Enter your name or player username.")
+    intro = "You get knocked out by -----. After an unknown period of time passes, your eyes don't decieve you--you find yourself laying next to a red portal, sluggishly getting the ability to move your muscles and limbs. A fellow townsperson runs up to you in fear.\n'Save our island!!'\nThe little boy runs away as your fuzzy brain gains awareness. You spot wooden posts nailed to trees with biome names; maybe they can be your guide...\n"
+    directions = f"{bold} - - DIRECTIONS - -{end}\nCollect scrolls from each biome to find the code word. Keep trap of clues and line up the number to the letter.\nWatch out for nighttime.\nDon't let your sanity deplete too far.\nScariness may be useful in combat.\nUse your brain.\nPerhaps don't be stupid."
+    # CHANGE
+    shadow_guardian = {
+        "hp": 800,
+        "dmg": 30,
+        "passive_dialogue": {
+            "1": "Hm.",
+            "2": "Oh..",},
+        "aggressive_dialogue": {
+            "1": "You're actually pathetic!",},
+        "attack_type": {
+            "normal": 1,
+            "shockwave": 2,
+            "shadow_clone": 3,
+            "stalactite ambush": 4,
+        }
+    }
+    # CHANGE
+    abyssal_echo = {
+        "hp": 260,
+        "dmg": 20,
+        "passive_dialogue": {
+            "1": "Hm.",
+            "2": "Oh..",},
+        "aggressive_dialogue": {
+            "1": "You're actually pathetic!",},
+        "attack_type": {
+            "normal": 1,
+            "ripples": 2,
+            "underwater": 3,
+            "wave": 4,
+        }
+    }
+    player = {
+        "name": playername,
+        "hp": 150,
+        "dmg": 22,
+        "sanity": 100,
+        "scariness": 1,
+        "inventory": []
+    }
+    status = {
+        "player": player,
+        "boss": shadow_guardian,
+        "mob": abyssal_echo,
+        "current_loc": current_loc
+    }
+    display_stats = f"{bold} - - PLAYER STATISTICS - - {end}\nUser: {bold}{player['name']}\n{end}Health: {bold}{player['hp']}{end}\nDamage: {bold}{player['dmg']}{end}\nSanity: {bold}{player['sanity']}{end}\nScariness: {bold}{player['scariness']}{end}"
+
+    print(intro)
+    time.sleep(10)
+    print(directions)
+    time.sleep(5)
+    print(display_stats)
+    time.sleep(5)
+    while True:
+        location = status["current_loc"]
+        print(f"You're in the {location_names[location]}")
+        time.sleep(1)
+        location_function = location_funcs[location]
+        location_function(status)
 
 def player_turn():
-
+    bold = '\033[1m'
+    end = '\033[0m'
 def monster_turn():
-
+    bold = '\033[1m'
+    end = '\033[0m'
 def spawn():
-
+    bold = '\033[1m'
+    end = '\033[0m'
 def grasslands():
-
+    bold = '\033[1m'
+    end = '\033[0m'
 def deciduous_forest():
-
+    bold = '\033[1m'
+    end = '\033[0m'
 def swamp():
-
+    bold = '\033[1m'
+    end = '\033[0m'
 def graveyard():
-
+    bold = '\033[1m'
+    end = '\033[0m'
 def desert():
-
+    bold = '\033[1m'
+    end = '\033[0m'
 def rockylands():
-
+    bold = '\033[1m'
+    end = '\033[0m'
 def stalagmite_terrain():
-
+    bold = '\033[1m'
+    end = '\033[0m'
 def ocean():
+    bold = '\033[1m'
+    end = '\033[0m'
 
 location_funcs = {
     Locations.SPAWN: spawn,
@@ -60,4 +138,7 @@ location_funcs = {
     Locations.DESERT: desert,
     Locations.ROCKYLANDS: rockylands,
     Locations.STALAGMITE_TERRAIN: stalagmite_terrain,
-    Locations.OCEAN: ocean}
+    Locations.OCEAN: ocean
+}
+
+main()
